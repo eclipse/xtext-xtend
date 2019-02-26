61bed5f138dffbe94e1c240d21643eaee6a52b8e
Karsten Thoms <karsten.thoms@itemis.de>
2019-02-26 13:32:53 +0100

    [#734] Make injector provider registry aware
    Take care of restoring global registries properly.
    Also converted the class back from Xtend to Java. Implementation is too
    trivial to justify Xtend usage.
    
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

---
+++ org.eclipse.xtend.ide.tests/src/org/eclipse/xtend/ide/tests/XtendIDEInjectorProvider.java
@@ -0,0 +1,42 @@
+package org.eclipse.xtend.ide.tests;
+
+import org.eclipse.xtend.ide.internal.XtendActivator;
+import org.eclipse.xtext.testing.GlobalRegistries;
+import org.eclipse.xtext.testing.GlobalRegistries.GlobalStateMemento;
+import org.eclipse.xtext.testing.IInjectorProvider;
+import org.eclipse.xtext.testing.IRegistryConfigurator;
+
+import com.google.inject.Injector;
+
+public class XtendIDEInjectorProvider implements IInjectorProvider, IRegistryConfigurator {
+
+	protected GlobalStateMemento stateBeforeInjectorCreation;
+	protected GlobalStateMemento stateAfterInjectorCreation;
+	protected Injector injector;
+
+	static {
+		GlobalRegistries.initializeDefaults();
+	}
+
+	@Override
+	public Injector getInjector() {
+		if (injector == null) {
+			stateBeforeInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
+			this.injector = XtendActivator.getInstance().getInjector(XtendActivator.ORG_ECLIPSE_XTEND_CORE_XTEND);
+			stateAfterInjectorCreation = GlobalRegistries.makeCopyOfGlobalState();
+		}
+		return injector;
+	}
+
+	@Override
+	public void restoreRegistry() {
+		stateBeforeInjectorCreation.restoreGlobalState();
+	}
+
+	@Override
+	public void setupRegistry() {
+		getInjector();
+		stateAfterInjectorCreation.restoreGlobalState();
+	}
+	
+}
