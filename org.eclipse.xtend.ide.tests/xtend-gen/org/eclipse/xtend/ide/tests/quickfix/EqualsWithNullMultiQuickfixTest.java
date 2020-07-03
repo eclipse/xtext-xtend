0d4ebde6411891e22c55d98b57de26d9a9713695
Aaron R Miller <aaron.miller@waweb.io>
2020-07-03 15:25:14 -0700

    Added EqualsWithNullMultiQuickfixTest class stub

---
+++ org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/quickfix/EqualsWithNullMultiQuickfixTest.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2020 itemis AG (http://www.itemis.eu) and others.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.xtend.ide.tests.quickfix;
+
+import org.eclipse.xtend.ide.tests.XtendIDEInjectorProvider;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.XtextRunner;
+import org.eclipse.xtext.ui.testing.AbstractMultiQuickfixTest;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Aaron R Miller - Initial contribution and API
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(XtendIDEInjectorProvider.class)
+@SuppressWarnings("all")
+public class EqualsWithNullMultiQuickfixTest extends AbstractMultiQuickfixTest {
+}
