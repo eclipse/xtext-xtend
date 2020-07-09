479f4c0e13064159b200dc35b9a94627ff7db418
Aaron R Miller <aaron.miller@waweb.io>
2020-07-08 22:34:37 -0700

    Added RemoveUnnecessaryModifiersMultiQuickfixTest
    1061: Provide ui test infrastructure for testing multi quickfixes [2/3]
    
    Task-Url: https://github.com/eclipse/xtext-xtend/issues/1061
    Signed-off-by: Aaron R Miller <aaron.miller@waweb.io>

--- org.eclipse.xtend.ide.tests/longrunning/src/org/eclipse/xtend/ide/tests/quickfix/RemoveUnnecessaryModifiersMultiQuickfixTest.xtend
+++ org.eclipse.xtend.ide.tests/longrunning/src/org/eclipse/xtend/ide/tests/quickfix/RemoveUnnecessaryModifiersMultiQuickfixTest.xtend
@@ -8,11 +8,19 @@
  *******************************************************************************/
 package org.eclipse.xtend.ide.tests.quickfix
 
+import com.google.inject.Inject
+import java.util.Arrays
 import org.eclipse.xtend.ide.tests.XtendIDEInjectorProvider
 import org.eclipse.xtext.testing.InjectWith
 import org.eclipse.xtext.testing.XtextRunner
+import org.eclipse.xtext.ui.editor.XtextEditor
+import org.eclipse.xtext.ui.editor.quickfix.QuickAssistCompletionProposal
+import org.eclipse.xtext.ui.refactoring.ui.SyncUtil
 import org.eclipse.xtext.ui.testing.AbstractMultiQuickfixTest
+import org.junit.Test
 import org.junit.runner.RunWith
+
+import static extension org.eclipse.xtend.ide.tests.WorkbenchTestHelper.createPluginProject
 
 /**
  * @author Aaron R Miller - Initial contribution and API
@@ -20,4 +28,94 @@ import org.junit.runner.RunWith
 @RunWith(XtextRunner)
 @InjectWith(XtendIDEInjectorProvider)
 class RemoveUnnecessaryModifiersMultiQuickfixTest extends AbstractMultiQuickfixTest {
+
+	static final String MODEL_WITH_UNNECESSARY_MODIFIERS = '''
+		package unnecessarymodifiers
+		public class Foo {
+			private final val A = 1
+			def public m() {
+				new Runnable {
+					public override def run() {}
+				}
+			}
+		}
+	'''
+
+	@Inject extension SyncUtil
+
+	XtextEditor xtextEditor
+
+	override String getFileName() {
+		return "Foo"
+	}
+
+	override dslFile(CharSequence content) {
+		super.dslFile(projectName, "src/unnecessarymodifiers/" + fileName, fileExtension, content);
+	}
+
+	override void setUp() throws Exception {
+		super.setUp()
+
+		projectName.createPluginProject()
+		xtextEditor = openEditor(dslFile("\n"))
+	}
+
+	@Test
+	def void testRemoveUnnecessaryModifiersQuickfixXtend() {
+
+		// Use Xtend style API
+		// //
+		
+		MODEL_WITH_UNNECESSARY_MODIFIERS.testMultiQuickfix('''
+			package unnecessarymodifiers
+			<0<public>0> class Foo {
+				<1<private>1> <2<final>2> val <3<A>3> = 1
+				def <4<public>4> m() {
+					new Runnable {
+						<5<public>5> override <6<def>6> run() {}
+					}
+				}
+			}
+			----------------------------------------------------
+			----------------------------------------------------
+			0: message=The public modifier is unnecessary on class Foo
+			1: message=The private modifier is unnecessary on field A
+			2: message=The final modifier is unnecessary on field A
+			3: message=The value of the field Foo.A is not used
+			4: message=The public modifier is unnecessary on method m
+			5: message=The public modifier is unnecessary on method run
+			6: message=The def modifier is unnecessary on method run
+		''', '''
+			package unnecessarymodifiers
+			class Foo {
+				val A = 1
+				def m() {
+					new Runnable {
+						override run() {}
+					}
+				}
+			}
+			-------------------
+			(no markers found)
+		''')
+	}
+
+	@Test
+	def void testRemoveUnnecessaryModifiersQuickfixJava() {
+
+		// Use Java style API
+		// //
+		
+		xtextEditor.document.set(MODEL_WITH_UNNECESSARY_MODIFIERS)
+		xtextEditor.waitForReconciler()
+
+		val offset = MODEL_WITH_UNNECESSARY_MODIFIERS.indexOf("==") + 1
+		val proposals = computeQuickAssistProposals(xtextEditor, offset)
+
+		// TODO java.lang.AssertionError: expected:<1> but was:<0>
+		assertEquals(1, proposals.size())
+		assertEquals(1, proposals.filter[it instanceof QuickAssistCompletionProposal].size())
+		
+		// TODO assert proposal text...
+	}
 }
--- org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/quickfix/RemoveUnnecessaryModifiersMultiQuickfixTest.java
+++ org.eclipse.xtend.ide.tests/xtend-gen/org/eclipse/xtend/ide/tests/quickfix/RemoveUnnecessaryModifiersMultiQuickfixTest.java
@@ -8,10 +8,26 @@
  */
 package org.eclipse.xtend.ide.tests.quickfix;
 
+import com.google.inject.Inject;
+import java.util.List;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.xtend.ide.tests.WorkbenchTestHelper;
 import org.eclipse.xtend.ide.tests.XtendIDEInjectorProvider;
+import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.XtextRunner;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.quickfix.QuickAssistCompletionProposal;
+import org.eclipse.xtext.ui.refactoring.ui.SyncUtil;
 import org.eclipse.xtext.ui.testing.AbstractMultiQuickfixTest;
+import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.lib.Functions.Function0;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -21,4 +37,152 @@ import org.junit.runner.RunWith;
 @InjectWith(XtendIDEInjectorProvider.class)
 @SuppressWarnings("all")
 public class RemoveUnnecessaryModifiersMultiQuickfixTest extends AbstractMultiQuickfixTest {
+  private static final String MODEL_WITH_UNNECESSARY_MODIFIERS = new Function0<String>() {
+    @Override
+    public String apply() {
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append("package unnecessarymodifiers");
+      _builder.newLine();
+      _builder.append("public class Foo {");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("private final val A = 1");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("def public m() {");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("new Runnable {");
+      _builder.newLine();
+      _builder.append("\t\t\t");
+      _builder.append("public override def run() {}");
+      _builder.newLine();
+      _builder.append("\t\t");
+      _builder.append("}");
+      _builder.newLine();
+      _builder.append("\t");
+      _builder.append("}");
+      _builder.newLine();
+      _builder.append("}");
+      _builder.newLine();
+      return _builder.toString();
+    }
+  }.apply();
+  
+  @Inject
+  @Extension
+  private SyncUtil _syncUtil;
+  
+  private XtextEditor xtextEditor;
+  
+  @Override
+  public String getFileName() {
+    return "Foo";
+  }
+  
+  @Override
+  public IFile dslFile(final CharSequence content) {
+    String _projectName = this.getProjectName();
+    String _fileName = this.getFileName();
+    String _plus = ("src/unnecessarymodifiers/" + _fileName);
+    return super.dslFile(_projectName, _plus, this.getFileExtension(), content);
+  }
+  
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    WorkbenchTestHelper.createPluginProject(this.getProjectName());
+    this.xtextEditor = this.openEditor(this.dslFile("\n"));
+  }
+  
+  @Test
+  public void testRemoveUnnecessaryModifiersQuickfixXtend() {
+    StringConcatenation _builder = new StringConcatenation();
+    _builder.append("package unnecessarymodifiers");
+    _builder.newLine();
+    _builder.append("<0<public>0> class Foo {");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("<1<private>1> <2<final>2> val <3<A>3> = 1");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("def <4<public>4> m() {");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("new Runnable {");
+    _builder.newLine();
+    _builder.append("\t\t\t");
+    _builder.append("<5<public>5> override <6<def>6> run() {}");
+    _builder.newLine();
+    _builder.append("\t\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("\t");
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("}");
+    _builder.newLine();
+    _builder.append("----------------------------------------------------");
+    _builder.newLine();
+    _builder.append("----------------------------------------------------");
+    _builder.newLine();
+    _builder.append("0: message=The public modifier is unnecessary on class Foo");
+    _builder.newLine();
+    _builder.append("1: message=The private modifier is unnecessary on field A");
+    _builder.newLine();
+    _builder.append("2: message=The final modifier is unnecessary on field A");
+    _builder.newLine();
+    _builder.append("3: message=The value of the field Foo.A is not used");
+    _builder.newLine();
+    _builder.append("4: message=The public modifier is unnecessary on method m");
+    _builder.newLine();
+    _builder.append("5: message=The public modifier is unnecessary on method run");
+    _builder.newLine();
+    _builder.append("6: message=The def modifier is unnecessary on method run");
+    _builder.newLine();
+    StringConcatenation _builder_1 = new StringConcatenation();
+    _builder_1.append("package unnecessarymodifiers");
+    _builder_1.newLine();
+    _builder_1.append("class Foo {");
+    _builder_1.newLine();
+    _builder_1.append("\t");
+    _builder_1.append("val A = 1");
+    _builder_1.newLine();
+    _builder_1.append("\t");
+    _builder_1.append("def m() {");
+    _builder_1.newLine();
+    _builder_1.append("\t\t");
+    _builder_1.append("new Runnable {");
+    _builder_1.newLine();
+    _builder_1.append("\t\t\t");
+    _builder_1.append("override run() {}");
+    _builder_1.newLine();
+    _builder_1.append("\t\t");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("\t");
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("}");
+    _builder_1.newLine();
+    _builder_1.append("-------------------");
+    _builder_1.newLine();
+    _builder_1.append("(no markers found)");
+    _builder_1.newLine();
+    this.testMultiQuickfix(RemoveUnnecessaryModifiersMultiQuickfixTest.MODEL_WITH_UNNECESSARY_MODIFIERS, _builder, _builder_1);
+  }
+  
+  @Test
+  public void testRemoveUnnecessaryModifiersQuickfixJava() {
+    this.xtextEditor.getDocument().set(RemoveUnnecessaryModifiersMultiQuickfixTest.MODEL_WITH_UNNECESSARY_MODIFIERS);
+    this._syncUtil.waitForReconciler(this.xtextEditor);
+    int _indexOf = RemoveUnnecessaryModifiersMultiQuickfixTest.MODEL_WITH_UNNECESSARY_MODIFIERS.indexOf("==");
+    final int offset = (_indexOf + 1);
+    final ICompletionProposal[] proposals = this.computeQuickAssistProposals(this.xtextEditor, offset);
+    Assert.assertEquals(1, ((List<ICompletionProposal>)Conversions.doWrapArray(proposals)).size());
+    final Function1<ICompletionProposal, Boolean> _function = (ICompletionProposal it) -> {
+      return Boolean.valueOf((it instanceof QuickAssistCompletionProposal));
+    };
+    Assert.assertEquals(1, IterableExtensions.size(IterableExtensions.<ICompletionProposal>filter(((Iterable<ICompletionProposal>)Conversions.doWrapArray(proposals)), _function)));
+  }
 }
